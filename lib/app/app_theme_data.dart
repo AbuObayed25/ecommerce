cabe3d2018ca713721061baf14cf6586d6f40ca7
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2025-01-12 12:55:55 +0600

    done

--- lib/app/app_theme_data.dart
+++ lib/app/app_theme_data.dart
@@ -4,20 +4,45 @@ import 'package:flutter/material.dart';
 class AppThemeData {
   static ThemeData get lightThemeData {
     return ThemeData(
-        colorSchemeSeed: AppColors.themecolor,
-        progressIndicatorTheme: const ProgressIndicatorThemeData(
-          color: AppColors.themecolor,
-        ),
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 2,
-        ),
-        textTheme: TextTheme(
-            titleLarge: TextStyle(
+      colorSchemeSeed: AppColors.themecolor,
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        color: AppColors.themecolor,
+      ),
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.white,
+        elevation: 2,
+      ),
+      textTheme: TextTheme(
+        titleLarge: TextStyle(
           fontSize: 28,
           fontWeight: FontWeight.w600,
-        )));
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        fillColor: Colors.white,
+        filled: true,
+        contentPadding: EdgeInsets.symmetric(horizontal: 16,vertical: 10),
+        hintStyle: TextStyle(
+          color: Colors.grey,
+          fontWeight: FontWeight.w400 ,
+        ),
+        border: OutlineInputBorder(
+          //borderRadius: BorderRadius.circular(8)
+          borderSide: BorderSide(color: AppColors.themecolor)
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.themecolor,
+            foregroundColor: Colors.white,
+            fixedSize: Size.fromWidth(double.maxFinite),
+            shape:RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            )
+        ),
+      )
+    );
   }
 
   static ThemeData get darkThemeData {
--- lib/features/auth/ui/screens/email_verification_screen.dart
+++ lib/features/auth/ui/screens/email_verification_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/app/app_colors.dart';
 import 'package:ecommerce/features/auth/ui/widgets/AppLogoWidget.dart';
 import 'package:flutter/material.dart';
 
@@ -16,25 +17,43 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 80,
-              ),
-              AppLogoWidget(),
-              SizedBox(
-                height: 24,
-              ),
-              Text('Welcome Back',style: Theme.of(context).textTheme.titleLarge,),
-              Text('Please Enter Your Email Address'),
-              SizedBox(height: 8,),
-              TextFormField(
-                
-              )
-            ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                AppLogoWidget(),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Welcome Back',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Text(
+                  'Please Enter Your Email Address',
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                        color: Colors.grey,
+                      ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Enter your email'),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(height: 8,),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Next'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
