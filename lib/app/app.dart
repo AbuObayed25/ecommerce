4e27f377ab1f0cafecf74a7c6bcf6adb4bdeee53
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2025-01-18 22:37:46 +0600

    updated

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,26 +1,33 @@
 import 'package:ecommerce/app/app_colors.dart';
 import 'package:ecommerce/app/app_theme_data.dart';
+import 'package:ecommerce/app/controller_binder.dart';
 import 'package:ecommerce/features/auth/ui/screens/complete_profile_screen.dart';
 import 'package:ecommerce/features/auth/ui/screens/email_verification_screen.dart';
 import 'package:ecommerce/features/auth/ui/screens/otp_verification_screen.dart';
 import 'package:ecommerce/features/auth/ui/screens/splash_screen.dart';
+import 'package:ecommerce/features/home/ui/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import '../features/common/ui/screens/main_bottom_nav_screen.dart';
 
 class CraftyBay extends StatelessWidget {
   const CraftyBay({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       initialRoute: '/',
       theme:AppThemeData.lightThemeData,
       darkTheme: AppThemeData.darkThemeData,
       themeMode: ThemeMode.system,
+      initialBinding: ControllerBinder(),
       routes: {
         '/':(context)=>const SplashScreen(),
         EmailVerificationScreen.name:(context)=>const EmailVerificationScreen(),
         OtpVerificationScreen.name:(context)=>const OtpVerificationScreen(),
         CompleteProfileScreen.name:(context)=>const CompleteProfileScreen(),
+        MainBottomNavScreen.name:(context)=>const MainBottomNavScreen(),
       },
     );
   }
