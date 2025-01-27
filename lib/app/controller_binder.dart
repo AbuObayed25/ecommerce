7c66c8ca0b47da48057a4a667b7640eea0ee6a96
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2025-01-27 23:52:52 +0600

    updated

--- lib/app/controller_binder.dart
+++ lib/app/controller_binder.dart
@@ -1,6 +1,6 @@
 import 'package:ecommerce/features/common/ui/controllers/main_bottom_nav_controller.dart';
 import 'package:get/get.dart';
-
+//manage the Get using Binder for routing
 class ControllerBinder extends Bindings {
   @override
   void dependencies() {
