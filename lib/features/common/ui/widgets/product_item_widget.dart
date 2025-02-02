a4e0cd3d6e3818d01e43d1e3c8f76f57ad0a7ab9
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2025-02-02 21:18:28 +0600

    updated

--- lib/features/common/ui/widgets/product_item_widget.dart
+++ lib/features/common/ui/widgets/product_item_widget.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/features/product/ui/screens/product_details_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../app/app_colors.dart';
@@ -9,87 +10,92 @@ class ProductItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 140,
-      child: Card(
-        color: Colors.white,
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                  color: AppColors.themecolor.withOpacity(.1),
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(16),
-                    topLeft: Radius.circular(16),
-                  )),
-              child: Image.asset(
-                'assets/images/nike_Shoe1.png',
-                width: 120,
-                height: 90,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, ProductDetailsScreen.name, arguments: 1);
+      },
+      child: SizedBox(
+        width: 140,
+        child: Card(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    color: AppColors.themecolor.withOpacity(.1),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(16),
+                      topLeft: Radius.circular(16),
+                    )),
+                child: Image.asset(
+                  'assets/images/nike_Shoe1.png',
+                  width: 120,
+                  height: 90,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                children: [
-                  Text(
-                    'Nike Latest Shoe Air',
-                    maxLines: 1,
-                    style: TextStyle(
-                      overflow: TextOverflow.ellipsis,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black54,
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Text(
+                      'Nike Latest Shoe Air',
+                      maxLines: 1,
+                      style: TextStyle(
+                        overflow: TextOverflow.ellipsis,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black54,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 2),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '\$100',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: AppColors.themecolor,
-                        ),
-                      ),
-                      Wrap(
-                        children: [
-                          Icon(
-                            Icons.star,
-                            size: 18,
-                            color: Colors.amber,
+                    const SizedBox(height: 2),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '\$100',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.themecolor,
                           ),
-                          Text(
-                            '4.5',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              color: AppColors.themecolor,
+                        ),
+                        Wrap(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 18,
+                              color: Colors.amber,
                             ),
+                            Text(
+                              '4.5',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: AppColors.themecolor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: AppColors.themecolor,
                           ),
-                        ],
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(2),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          color: AppColors.themecolor,
+                          child: Icon(
+                            Icons.favorite_border,
+                            size: 16,
+                            color: Colors.white,
+                          ),
                         ),
-                        child: Icon(
-                          Icons.favorite_border,
-                          size: 16,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                ],
-              ),
-            )
-          ],
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
