9724a70d6709d6fef742136023744331e42a710f
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2025-02-10 21:27:36 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'app/app.dart';
-
+//When you use device preview it is easy to understand the mobile frame
 void main() {
   runApp(
     DevicePreview(
