a71789b2aab34c0a5f56110e0613ee2a15a15566
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2025-02-26 20:02:55 +0600

    updated

--- lib/service/network_caller/network_caller.dart
+++ lib/service/network_caller/network_caller.dart
@@ -17,13 +17,14 @@ class NetworkResponse {
 }
 
 class NetworkCaller {
-  final Logger _logger=Logger();
+  final Logger _logger = Logger();
+
   Future<NetworkResponse> getRequest(String url) async {
     try {
       Uri uri = Uri.parse(url);
-      _logger.i("URL=>$url");
+      _logRequest(url);
       Response response = await get(uri);
-      _logger.i("Status=>$url");
+      _logResponse(url, response.statusCode, response.headers, response.body);
       if (response.statusCode == 200) {
         final decodedMessage = jsonDecode(response.body);
         return NetworkResponse(
@@ -37,6 +38,8 @@ class NetworkCaller {
         );
       }
     } catch (e) {
+      _logResponse(url, -1, null, '', e.toString());
+
       return NetworkResponse(
         isSuccess: false,
         statusCode: -1,
@@ -44,7 +47,49 @@ class NetworkCaller {
       );
     }
   }
-  void _logRequest(String,url,[Map<String,dynamic>? headers,Map<String,dynamic>?body ]){
-    _logger.i(message)
+  Future<NetworkResponse> postRequest(String url, {Map<String, dynamic>? body}) async {
+    try {
+      Uri uri = Uri.parse(url);
+      Map<String,String>headers= {'content-type': 'application/json'};
+      _logRequest(url,headers,body);
+      Response response = await post(uri,headers: headers,body: jsonEncode(body));
+      _logResponse(url, response.statusCode, response.headers, response.body);
+      if (response.statusCode == 200) {
+        final decodedMessage = jsonDecode(response.body);
+        return NetworkResponse(
+            isSuccess: true,
+            statusCode: response.statusCode,
+            responseData: decodedMessage);
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      _logResponse(url, -1, null, '', e.toString());
+
+      return NetworkResponse(
+        isSuccess: false,
+        statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  void _logRequest(String url,
+      [Map<String, dynamic>? headers, Map<String, dynamic>? body]) {
+    _logger.i('URL=>$url\nHEADERS=>$headers\nBODY');
+  }
+
+  void _logResponse(
+      String url, int statusCode, Map<String, String>? headers, String body,
+      [String? errorMessage]) {
+    if (errorMessage != null) {
+      _logger.e('URL=>$url\n errorMessage=>$errorMessage');
+    } else {
+      _logger.i(
+          'URL=>$url\nHEADERS=>$headers\nSTATUS CODE=>$statusCode\nBODY=>$body');
+    }
   }
 }
