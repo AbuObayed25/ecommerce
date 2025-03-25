4e0a46fd91511d078193806fb72663208919466b
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2025-03-25 11:50:55 +0600

    updated

--- lib/app/controller_binder.dart
+++ lib/app/controller_binder.dart
@@ -1,9 +1,13 @@
+import 'package:ecommerce/features/auth/ui/controllers/email_verification_controller.dart';
 import 'package:ecommerce/features/common/ui/controllers/main_bottom_nav_controller.dart';
+import 'package:ecommerce/service/network_caller/network_caller.dart';
 import 'package:get/get.dart';
 //manage the Get using Binder for routing
 class ControllerBinder extends Bindings {
   @override
   void dependencies() {
     Get.put(MainBottomNavController());
+    Get.put(NetworkCaller());
+    Get.put(EmailVerificationController());
   }
 }
